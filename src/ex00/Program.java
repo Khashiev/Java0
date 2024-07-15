3b69fb5263d2a5c9ed5bbb11466a2b86b115c024
Khashiev <nxashiev@gmail.com>
2024-07-15 21:39:08 +0300

    ex00

---
+++ src/ex00/Program.java
@@ -0,0 +1,15 @@
+package src.ex00;
+
+public class Program {
+    public static void main(String[] args) {
+        int number = 479598;
+        int sum = 0;
+
+        for (int i = 0; i < 6; i++) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        System.out.println(sum);
+    }
+}
